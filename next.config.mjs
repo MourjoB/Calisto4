14c35a5fe977054b5e5d554c806f828567741597
MourjoB <mourjobosu@gmail.com>
2025-01-09 19:30:38 +0530

    Your commit message

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,    // Required for static export
+    },
+    // If you're using basePath, add it here
+    // basePath: '',
+};
 
 export default nextConfig;
